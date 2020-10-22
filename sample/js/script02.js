864a3626a9ba968751a8e49daaa5a1743d4a4d3e
my-0998 <68629457+my-0998@users.noreply.github.com>
2020-10-22 23:39:27 +0900

    syuusei

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -7,23 +7,23 @@ $(function(){
 
 
     $("#button01").on("click",function(){
-        $("#content").Text("こんにちは!");
+        $("#content").text("こんにちは!");
       })
       
-      $("#button02").on("click",function(){
-        $("#content").Text(txt1);
+    $("#button02").on("click",function(){
+        $("#content").text(txt1);
       })
 
-      $("#button03").on("click",function(){
-        $("#content").Text(txt1 + txt2);
+    $("#button03").on("click",function(){
+        $("#content").text(txt1 + txt2);
       })
 
-      $("#button04").on("click",function(){
-        $("#content").Text(num1 + num2);
+    $("#button04").on("click",function(){
+        $("#content").text(num1 + num2);
       })
 
-      $("#button05").on("click",function(){
-        $("#content").Text(txt1 　+　(num1 + num2) +  "です");
+    $("#button05").on("click",function(){
+        $("#content").text(num1 +　"たす" +　num2 + "は" + (num1 + num2) +  "です");
       })
 
 });
